8ee3b3298a7930dc6ca2be71dab515a61d84d2ff
Linnea Strid <stridlinnea@gmail.com>
2015-02-17 17:45:09 +0000

    work complete missing images

--- app.rb
+++ app.rb
@@ -28,6 +28,31 @@ class App < Sinatra::Base
     erb :work
   end
 
+  get "/work/matterhorn" do
+    erb "work/matterhorn".to_sym
+  end
+  get "/work/ubiquity-press-portals" do
+    erb "work/ubiquity_press_portals".to_sym
+  end
+  get "/work/ubiquity-press-journals" do
+    erb "work/ubiquity_press_journals".to_sym
+  end
+  get "/work/serious-fox" do
+    erb "work/serious_fox".to_sym
+  end
+  get "/work/evolve-my-retirement" do
+    erb "work/evolve_my_retirement".to_sym
+  end
+  get "/work/metatrails" do
+    erb "work/metatrails".to_sym
+  end
+  get "/work/tottenham-hotspur" do
+    erb "work/tottenham_hotspur".to_sym
+  end
+  get "/work/tech-city-uk" do
+    erb "work/tech_city_uk".to_sym
+  end
+
   get "/contact" do
     erb :contact
   end
